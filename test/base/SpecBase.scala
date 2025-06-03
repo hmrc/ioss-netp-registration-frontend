f8bedb026644fb8da407c2afb8cc134b528679d8
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-06-03 13:36:20 +0100

    update SpecBase for controller tests

--- test/base/SpecBase.scala
+++ test/base/SpecBase.scala
@@ -18,7 +18,10 @@ package base
 
 import controllers.actions.*
 import generators.Generators
-import models.UserAnswers
+import models.{DesAddress, UserAnswers}
+import models.domain.ModelHelpers.normaliseSpaces
+import models.domain.VatCustomerInfo
+import org.scalacheck.Arbitrary
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
@@ -29,20 +32,37 @@ import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.FakeRequest
 
+import java.time.{Clock, Instant, LocalDate, ZoneId}
+
 trait SpecBase
   extends AnyFreeSpec
     with Matchers
     with TryValues
     with OptionValues
     with ScalaFutures
-    with IntegrationPatience 
+    with IntegrationPatience
     with Generators {
 
   val userAnswersId: String = "id"
 
-  def emptyUserAnswers : UserAnswers = UserAnswers(userAnswersId)
+  def emptyUserAnswers : UserAnswers = UserAnswers(userAnswersId, lastUpdated = arbitraryInstant)
 
   def messages(app: Application): Messages = app.injector.instanceOf[MessagesApi].preferred(FakeRequest())
+  
+  def emptyUserAnswersWithVatInfo: UserAnswers = emptyUserAnswers.copy(vatInfo = Some(vatCustomerInfo))
+  
+  val arbitraryInstant: Instant = arbitraryDate.arbitrary.sample.value.atStartOfDay(ZoneId.systemDefault()).toInstant
+  val stubClockAtArbitraryDate: Clock = Clock.fixed(arbitraryInstant, ZoneId.systemDefault())
+
+  val vatCustomerInfo: VatCustomerInfo =
+    VatCustomerInfo(
+      registrationDate = LocalDate.now(stubClockAtArbitraryDate),
+      desAddress = arbitraryDesAddress.arbitrary.sample.value,
+      organisationName = Some("Company name"),
+      individualName = None,
+      singleMarketIndicator = true
+    )
+  
 
   protected def applicationBuilder(userAnswers: Option[UserAnswers] = None): GuiceApplicationBuilder =
     new GuiceApplicationBuilder()
