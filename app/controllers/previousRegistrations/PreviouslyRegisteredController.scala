76ecc751ba0f6b952ca328f867ecf54429b4574e
Tayamul <100432502+Tayamul@users.noreply.github.com>
2025-06-10 10:10:45 +0100

    VEI-211: Build the first page of the NETP Previous Registrations section.

--- app/controllers/previousRegistrations/PreviouslyRegisteredController.scala
+++ app/controllers/previousRegistrations/PreviouslyRegisteredController.scala
@@ -1,15 +1,32 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.previousRegistrations
 
 import controllers.actions.*
 import forms.previousRegistrations.PreviouslyRegisteredFormProvider
-import models.Mode
+import models.UserAnswers
 import pages.previousRegistrations.PreviouslyRegisteredPage
-import pages.{Waypoint, Waypoints}
+import pages.Waypoints
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.PreviouslyRegisteredView
+import views.html.previousRegistrations.PreviouslyRegisteredView
+import utils.FutureSyntax._
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
@@ -17,7 +34,6 @@ import scala.concurrent.{ExecutionContext, Future}
 class PreviouslyRegisteredController @Inject()(
                                          override val messagesApi: MessagesApi,
                                          sessionRepository: SessionRepository,
-                                         navigator: Navigator,
                                          identify: IdentifierAction,
                                          getData: DataRetrievalAction,
                                          requireData: DataRequiredAction,
@@ -26,31 +42,32 @@ class PreviouslyRegisteredController @Inject()(
                                          view: PreviouslyRegisteredView
                                  )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
-  val form = formProvider()
+  private val form = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad(waypoints: Waypoints): Action[AnyContent] = (identify andThen getData) {
     implicit request =>
+      val userAnswers = request.userAnswers.getOrElse(UserAnswers(request.userId))
 
-      val preparedForm = request.userAnswers.get(PreviouslyRegisteredPage) match {
+      val preparedForm = userAnswers.get(PreviouslyRegisteredPage) match {
         case None => form
         case Some(value) => form.fill(value)
       }
 
-      Ok(view(preparedForm, mode))
+      Ok(view(preparedForm, waypoints))
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+  def onSubmit(waypoints: Waypoints): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
 
       form.bindFromRequest().fold(
         formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
+          BadRequest(view(formWithErrors, waypoints)).toFuture,
 
         value =>
           for {
             updatedAnswers <- Future.fromTry(request.userAnswers.set(PreviouslyRegisteredPage, value))
             _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(PreviouslyRegisteredPage, mode, updatedAnswers))
+          } yield Redirect(PreviouslyRegisteredPage.navigate(waypoints, request.userAnswers, updatedAnswers).route)
       )
   }
 }
