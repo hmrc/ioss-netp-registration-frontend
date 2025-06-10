548bbdd540991aa58baa94c93decf048ac7d906f
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-06-10 15:05:41 +0100

    add form validator in EuCountryFormProvider

--- app/forms/vatEuDetails/EuCountryFormProvider.scala
+++ app/forms/vatEuDetails/EuCountryFormProvider.scala
@@ -17,15 +17,19 @@
 package forms.vatEuDetails
 
 import forms.mappings.Mappings
-import models.EuCountry
+import models.{Country, Index}
 import play.api.data.Form
 
 import javax.inject.Inject
 
 class EuCountryFormProvider @Inject() extends Mappings {
-
-  def apply(): Form[EuCountry] =
+  
+  def apply(countryIndex: Index, existingAnswers: Seq[Country]): Form[Country] = {
     Form(
-      "value" -> enumerable[EuCountry]("euCountry.error.required")
+      "value" -> text("euCountry.error.required")
+        .verifying("euCountry.error.required", value => Country.euCountries.exists(_.code == value))
+        .transform[Country](value => Country.euCountries.find(_.code == value).get, _.code)
+        .verifying(notADuplicate(countryIndex, existingAnswers, "euCountry.error.duplicate"))
     )
+  }
 }
