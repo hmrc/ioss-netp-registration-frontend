0d9c6bc3a7f692ff52736b74fbefce5a06609fc6
Tayamul <100432502+Tayamul@users.noreply.github.com>
2025-06-10 10:10:45 +0100

    VEI-211: Build the first page of the NETP Previous Registrations section.

--- test/controllers/previousRegistrations/PreviouslyRegisteredControllerSpec.scala
+++ test/controllers/previousRegistrations/PreviouslyRegisteredControllerSpec.scala
@@ -1,30 +1,39 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.previousRegistrations
 
 import base.SpecBase
-import controllers.routes
 import forms.previousRegistrations.PreviouslyRegisteredFormProvider
-import models.{NormalMode, UserAnswers}
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
+import pages.{EmptyWaypoints, Waypoints}
 import pages.previousRegistrations.PreviouslyRegisteredPage
-import play.api.inject.bind
-import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
-import repositories.SessionRepository
-import views.html.PreviouslyRegisteredView
+import views.html.previousRegistrations.PreviouslyRegisteredView
 
-import scala.concurrent.Future
 
 class PreviouslyRegisteredControllerSpec extends SpecBase with MockitoSugar {
 
-  def onwardRoute = Call("GET", "/foo")
+  private val waypoints: Waypoints = EmptyWaypoints
 
-  val formProvider = new PreviouslyRegisteredFormProvider()
-  val form = formProvider()
+  private val formProvider = new PreviouslyRegisteredFormProvider()
+  private val form = formProvider()
 
-  lazy val previouslyRegisteredRoute = routes.PreviouslyRegisteredController.onPageLoad(NormalMode).url
+  private lazy val previouslyRegisteredRoute = controllers.previousRegistrations.routes.PreviouslyRegisteredController.onPageLoad(waypoints).url
 
   "PreviouslyRegistered Controller" - {
 
@@ -40,13 +49,13 @@ class PreviouslyRegisteredControllerSpec extends SpecBase with MockitoSugar {
         val view = application.injector.instanceOf[PreviouslyRegisteredView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form, waypoints)(request, messages(application)).toString
       }
     }
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(PreviouslyRegisteredPage, true).success.value
+      val userAnswers = emptyUserAnswers.set(PreviouslyRegisteredPage, true).success.value
 
       val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
@@ -58,33 +67,7 @@ class PreviouslyRegisteredControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(true), NormalMode)(request, messages(application)).toString
-      }
-    }
-
-    "must redirect to the next page when valid data is submitted" in {
-
-      val mockSessionRepository = mock[SessionRepository]
-
-      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
-
-      val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers))
-          .overrides(
-            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
-            bind[SessionRepository].toInstance(mockSessionRepository)
-          )
-          .build()
-
-      running(application) {
-        val request =
-          FakeRequest(POST, previouslyRegisteredRoute)
-            .withFormUrlEncodedBody(("value", "true"))
-
-        val result = route(application, request).value
-
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual onwardRoute.url
+        contentAsString(result) mustEqual view(form.fill(true), waypoints)(request, messages(application)).toString
       }
     }
 
@@ -104,21 +87,7 @@ class PreviouslyRegisteredControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
-      }
-    }
-
-    "must redirect to Journey Recovery for a GET if no existing data is found" in {
-
-      val application = applicationBuilder(userAnswers = None).build()
-
-      running(application) {
-        val request = FakeRequest(GET, previouslyRegisteredRoute)
-
-        val result = route(application, request).value
-
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        contentAsString(result) mustEqual view(boundForm, waypoints)(request, messages(application)).toString
       }
     }
 
@@ -134,7 +103,7 @@ class PreviouslyRegisteredControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
       }
     }
   }
