0a4e3df1e40eb438d8d0d1d8cc75803d1cda0b1f
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-06-18 11:54:35 +0100

    add HasFixedEstablishment tests

--- test/controllers/vatEuDetails/HasFixedEstablishmentControllerSpec.scala
+++ test/controllers/vatEuDetails/HasFixedEstablishmentControllerSpec.scala
@@ -1,36 +1,39 @@
 package controllers.vatEuDetails
 
 import base.SpecBase
-import controllers.routes
-import forms.HasFixedEstablishmentFormProvider
-import models.{NormalMode, UserAnswers}
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.when
+import forms.vatEuDetails.HasFixedEstablishmentFormProvider
+import models.{Country, UserAnswers}
+import org.mockito.ArgumentMatchers.{any, eq as eqTo}
+import org.mockito.Mockito.{times, verify, when}
 import org.scalatestplus.mockito.MockitoSugar
-import pages.HasFixedEstablishmentPage
+import pages.JourneyRecoveryPage
+import pages.vatEuDetails.{EuCountryPage, HasFixedEstablishmentPage, VatRegisteredInEuPage}
+import play.api.data.Form
 import play.api.inject.bind
-import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
 import repositories.SessionRepository
-import views.html.HasFixedEstablishmentView
-
-import scala.concurrent.Future
+import utils.FutureSyntax.FutureOps
+import views.html.vatEuDetails.HasFixedEstablishmentView
 
 class HasFixedEstablishmentControllerSpec extends SpecBase with MockitoSugar {
 
-  def onwardRoute = Call("GET", "/foo")
+  private val country: Country = arbitraryCountry.arbitrary.sample.value
 
-  val formProvider = new HasFixedEstablishmentFormProvider()
-  val form = formProvider()
+  private val updatedAnswers: UserAnswers = emptyUserAnswersWithVatInfo
+    .set(VatRegisteredInEuPage, true).success.value
+    .set(EuCountryPage(countryIndex(0)), country).success.value
 
-  lazy val hasFixedEstablishmentRoute = routes.HasFixedEstablishmentController.onPageLoad(NormalMode).url
+  private val formProvider = new HasFixedEstablishmentFormProvider()
+  private val form: Form[Boolean] = formProvider(country)
+
+  private lazy val hasFixedEstablishmentRoute: String = routes.HasFixedEstablishmentController.onPageLoad(waypoints, countryIndex(0)).url
 
   "HasFixedEstablishment Controller" - {
 
     "must return OK and the correct view for a GET" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(updatedAnswers)).build()
 
       running(application) {
         val request = FakeRequest(GET, hasFixedEstablishmentRoute)
@@ -39,14 +42,14 @@ class HasFixedEstablishmentControllerSpec extends SpecBase with MockitoSugar {
 
         val view = application.injector.instanceOf[HasFixedEstablishmentView]
 
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+        status(result) `mustBe` OK
+        contentAsString(result) `mustBe` view(form, waypoints, countryIndex(0), country)(request, messages(application)).toString
       }
     }
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(HasFixedEstablishmentPage, true).success.value
+      val userAnswers = updatedAnswers.set(HasFixedEstablishmentPage(countryIndex(0)), true).success.value
 
       val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
@@ -57,21 +60,20 @@ class HasFixedEstablishmentControllerSpec extends SpecBase with MockitoSugar {
 
         val result = route(application, request).value
 
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(true), NormalMode)(request, messages(application)).toString
+        status(result) `mustBe` OK
+        contentAsString(result) `mustBe` view(form.fill(true), waypoints, countryIndex(0), country)(request, messages(application)).toString
       }
     }
 
-    "must redirect to the next page when valid data is submitted" in {
+    "must save the answer and redirect to the next page when valid data is submitted" in {
 
       val mockSessionRepository = mock[SessionRepository]
 
-      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+      when(mockSessionRepository.set(any())) thenReturn true.toFuture
 
       val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        applicationBuilder(userAnswers = Some(updatedAnswers))
           .overrides(
-            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
             bind[SessionRepository].toInstance(mockSessionRepository)
           )
           .build()
@@ -83,14 +85,19 @@ class HasFixedEstablishmentControllerSpec extends SpecBase with MockitoSugar {
 
         val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual onwardRoute.url
+        val expectedAnswers: UserAnswers = updatedAnswers
+          .set(HasFixedEstablishmentPage(countryIndex(0)), true).success.value
+
+        status(result) `mustBe` SEE_OTHER
+        redirectLocation(result).value `mustBe` HasFixedEstablishmentPage(countryIndex(0))
+          .navigate(waypoints, updatedAnswers, expectedAnswers).url
+        verify(mockSessionRepository, times(1)).set(eqTo(expectedAnswers))
       }
     }
 
     "must return a Bad Request and errors when invalid data is submitted" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(updatedAnswers)).build()
 
       running(application) {
         val request =
@@ -103,8 +110,8 @@ class HasFixedEstablishmentControllerSpec extends SpecBase with MockitoSugar {
 
         val result = route(application, request).value
 
-        status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+        status(result) `mustBe` BAD_REQUEST
+        contentAsString(result) `mustBe` view(boundForm, waypoints, countryIndex(0), country)(request, messages(application)).toString
       }
     }
 
@@ -117,8 +124,8 @@ class HasFixedEstablishmentControllerSpec extends SpecBase with MockitoSugar {
 
         val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        status(result) `mustBe` SEE_OTHER
+        redirectLocation(result).value `mustBe` JourneyRecoveryPage.route(waypoints).url
       }
     }
 
@@ -133,8 +140,8 @@ class HasFixedEstablishmentControllerSpec extends SpecBase with MockitoSugar {
 
         val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        status(result) `mustBe` SEE_OTHER
+        redirectLocation(result).value `mustBe` JourneyRecoveryPage.route(waypoints).url
       }
     }
   }
