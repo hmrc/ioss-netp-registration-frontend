9a7116a633f8a2f58b845b51464185d5390d784c
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-06-20 16:43:22 +0100

    add RegistrationTypeFormProvider tests

--- test/forms/vatEuDetails/RegistrationTypeFormProviderSpec.scala
+++ test/forms/vatEuDetails/RegistrationTypeFormProviderSpec.scala
@@ -1,29 +1,63 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package forms.vatEuDetails
 
 import forms.behaviours.OptionFieldBehaviours
-import models.RegistrationType
-import play.api.data.FormError
+import models.{Country, RegistrationType}
+import play.api.data.{Form, FormError}
 
 class RegistrationTypeFormProviderSpec extends OptionFieldBehaviours {
 
-  val form = new RegistrationTypeFormProvider()()
+  private val country: Country = arbitraryCountry.arbitrary.sample.value
+  private val requiredKey: String = "registrationType.error.required"
+
+  private val form: Form[RegistrationType] = new RegistrationTypeFormProvider()(country)
 
   ".value" - {
 
     val fieldName = "value"
-    val requiredKey = "registrationType.error.required"
 
     behave like optionsField[RegistrationType](
       form,
       fieldName,
-      validValues  = RegistrationType.values,
-      invalidError = FormError(fieldName, "error.invalid")
+      validValues = RegistrationType.values,
+      invalidError = FormError(fieldName, "error.invalid", args = Seq(country.name))
     )
 
     behave like mandatoryField(
       form,
       fieldName,
-      requiredError = FormError(fieldName, requiredKey)
+      requiredError = FormError(fieldName, requiredKey, args = Seq(country.name))
     )
   }
 }
