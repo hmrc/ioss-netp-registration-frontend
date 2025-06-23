6faf74d35d48fe1811eca29da9a8f04183de9dc2
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-06-23 12:03:53 +0100

    add EuVatNumberConstraints

--- app/forms/mappings/EuVatNumberConstraints.scala
+++ app/forms/mappings/EuVatNumberConstraints.scala
@@ -21,32 +21,32 @@ import play.api.data.validation.{Constraint, Invalid, Valid}
 
 trait EuVatNumberConstraints {
 
-  def validateEuVatNumberOrEu(countryCode: String, errorKey: String): Constraint[String] =
+  def validateEuVatNumber(countryCode: String, errorKey: String): Constraint[String] = {
     Constraint {
       input =>
-        if (checkEuRegistrationNumber(input)) {
-          Valid
-        } else if (matchesCountryRegex(input, countryCode)) {
+        if (matchesCountryRegex(input, countryCode)) {
           Valid
         } else {
           Invalid(errorKey)
         }
     }
+  }
 
-  private def checkEuRegistrationNumber(input: String): Boolean =
-    val regex = """^(?i)EU[0-9]{9}$"""
-    input.matches(regex)
-
-  private def matchesCountryRegex(input: String, countryCode: String): Boolean =
-    val regex = getCountryVatRegex(countryCode)
-    input.matches(regex)
-
-
-  private def getCountryVatRegex(countryCode: String): String =
+  private def getCountryVatRegex(countryCode: String): String = {
     CountryWithValidationDetails.euCountriesWithVRNValidationRules.find(_.country.code == countryCode)
     match {
       case Some(countryWithValidationDetails) =>
         countryWithValidationDetails.vrnRegex
       case _ => throw new Exception("invalid country code")
     }
+  }
+
+  private def checkEuRegistrationNumber(input: String): Boolean =
+    val regex = """^(?i)EU[0-9]{9}$"""
+    input.matches(regex)
+
+  private def matchesCountryRegex(input: String, countryCode: String): Boolean = {
+    val regex = getCountryVatRegex(countryCode)
+    input.matches(regex)
+  }
 }
