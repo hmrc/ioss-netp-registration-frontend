a8756f6f989c8c47d71bd7527e1091e793799071
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-06-24 16:27:39 +0100

    add waypoints, countryIndex for EuVatNumberController

--- app/controllers/vatEuDetails/EuVatNumberController.scala
+++ app/controllers/vatEuDetails/EuVatNumberController.scala
@@ -1,56 +1,93 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.vatEuDetails
 
+import controllers.GetCountry
 import controllers.actions.*
 import forms.vatEuDetails.EuVatNumberFormProvider
-import models.Mode
+import models.{CountryWithValidationDetails, Index}
 import pages.vatEuDetails.EuVatNumberPage
-import pages.{Waypoint, Waypoints}
+import pages.Waypoints
+import play.api.data.Form
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.EuVatNumberView
+import views.html.vatEuDetails.EuVatNumberView
+import utils.FutureSyntax.FutureOps
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class EuVatNumberController @Inject()(
-                                        override val messagesApi: MessagesApi,
-                                        sessionRepository: SessionRepository,
-                                        navigator: Navigator,
-                                        identify: IdentifierAction,
-                                        getData: DataRetrievalAction,
-                                        requireData: DataRequiredAction,
-                                        formProvider: EuVatNumberFormProvider,
-                                        val controllerComponents: MessagesControllerComponents,
-                                        view: EuVatNumberView
-                                      )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+                                       override val messagesApi: MessagesApi,
+                                       sessionRepository: SessionRepository,
+                                       identify: IdentifierAction,
+                                       getData: DataRetrievalAction,
+                                       requireData: DataRequiredAction,
+                                       formProvider: EuVatNumberFormProvider,
+                                       val controllerComponents: MessagesControllerComponents,
+                                       view: EuVatNumberView
+                                     )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport with GetCountry {
 
-  val form = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad(waypoints: Waypoints, countryIndex: Index): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
 
-      val preparedForm = request.userAnswers.get(EuVatNumberPage) match {
-        case None => form
-        case Some(value) => form.fill(value)
-      }
+      getCountryWithIndex(waypoints, countryIndex) { country =>
 
-      Ok(view(preparedForm, mode))
+        val form: Form[String] = formProvider(country)
+
+        val preparedForm = request.userAnswers.get(EuVatNumberPage(countryIndex)) match {
+          case None => form
+          case Some(value) => form.fill(value)
+        }
+
+        CountryWithValidationDetails.euCountriesWithVRNValidationRules.filter(_.country.code == country.code).head match {
+          case countryWithValidationDetails: CountryWithValidationDetails =>
+
+            Ok(view(preparedForm, waypoints, countryIndex, countryWithValidationDetails)).toFuture
+        }
+      }
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+  def onSubmit(waypoints: Waypoints, countryIndex: Index): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
 
-      form.bindFromRequest().fold(
-        formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
+      getCountryWithIndex(waypoints, countryIndex) { country =>
 
-        value =>
-          for {
-            updatedAnswers <- Future.fromTry(request.userAnswers.set(EuVatNumberPage, value))
-            _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(EuVatNumberPage, mode, updatedAnswers))
-      )
+        CountryWithValidationDetails.euCountriesWithVRNValidationRules.filter(_.country.code == country.code).head match {
+          case countryWithValidationDetails: CountryWithValidationDetails =>
+
+            val form: Form[String] = formProvider(country)
+
+            form.bindFromRequest().fold(
+              formWithErrors =>
+                BadRequest(view(formWithErrors, waypoints, countryIndex, countryWithValidationDetails)).toFuture,
+
+              euVrn =>
+                for {
+                  updatedAnswers <- Future.fromTry(request.userAnswers.set(EuVatNumberPage(countryIndex), euVrn))
+                  _ <- sessionRepository.set(updatedAnswers)
+                } yield Redirect(EuVatNumberPage(countryIndex).navigate(waypoints, request.userAnswers, updatedAnswers).route)
+
+
+            )
+        }
+      }
   }
 }
