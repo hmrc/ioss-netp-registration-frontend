9e8a0da52865ad04e12575d0db2777d6b7f883ea
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-06-24 16:31:19 +0100

    add waypoints, countryIndex to EuVatNumberSummary

--- app/viewmodels/checkAnswers/vatEuDetails/EuVatNumberSummary.scala
+++ app/viewmodels/checkAnswers/vatEuDetails/EuVatNumberSummary.scala
@@ -1,26 +1,48 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package viewmodels.checkAnswers.vatEuDetails
 
-import controllers.routes
-import models.{CheckMode, UserAnswers}
+import models.{Index, UserAnswers}
+import pages.{CheckAnswersPage, Waypoints}
 import pages.vatEuDetails.EuVatNumberPage
 import play.api.i18n.Messages
+import play.twirl.api.HtmlFormat
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
 import viewmodels.govuk.summarylist.*
 import viewmodels.implicits.*
 
-object EuVatNumberSummary  {
+object EuVatNumberSummary {
 
-  def row(answers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
-    answers.get(EuVatNumberPage).map {
-      answer =>
+  def row(
+           waypoints: Waypoints,
+           answers: UserAnswers,
+           countryIndex: Index,
+           sourcePage: CheckAnswersPage
+         )(implicit messages: Messages): Option[SummaryListRow] = {
+    answers.get(EuVatNumberPage(countryIndex)).map { answer =>
 
-        SummaryListRowViewModel(
-          key     = "euVatNumber.checkYourAnswersLabel",
-          value   = ValueViewModel(answer.toString),
-          actions = Seq(
-            ActionItemViewModel("site.change", routes.EuVatNumberController.onPageLoad(CheckMode).url)
-              .withVisuallyHiddenText(messages("euVatNumber.change.hidden"))
-          )
+      SummaryListRowViewModel(
+        key = "euVatNumber.checkYourAnswersLabel",
+        value = ValueViewModel(HtmlFormat.escape(answer).toString),
+        actions = Seq(
+          ActionItemViewModel("site.change", EuVatNumberPage(countryIndex).changeLink(waypoints, sourcePage).url)
+            .withVisuallyHiddenText(messages("euVatNumber.change.hidden"))
         )
+      )
     }
+  }
 }
