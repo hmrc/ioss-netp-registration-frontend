744332adc75603357be6a06306535370fa1b9550
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-06-25 17:15:19 +0100

    add EuVatController tests

--- test/controllers/vatEuDetails/EuVatNumberControllerSpec.scala
+++ test/controllers/vatEuDetails/EuVatNumberControllerSpec.scala
@@ -14,40 +14,49 @@
  * limitations under the License.
  */
 
-package controllers
+package controllers.vatEuDetails
 
 import base.SpecBase
-import forms.EuVatNumberFormProvider
-import models.{NormalMode, UserAnswers}
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.when
+import forms.vatEuDetails.EuVatNumberFormProvider
+import models.{Country, CountryWithValidationDetails, UserAnswers}
+import org.mockito.ArgumentMatchers.{any, eq as eqTo}
+import org.mockito.Mockito.{times, verify, when}
 import org.scalatestplus.mockito.MockitoSugar
-import pages.EuVatNumberPage
+import pages.JourneyRecoveryPage
+import pages.vatEuDetails.{EuCountryPage, EuVatNumberPage, VatRegisteredInEuPage}
+import play.api.Application
+import play.api.data.Form
 import play.api.inject.bind
-import play.api.mvc.Call
-import play.api.test.FakeRequest
-import play.api.test.Helpers._
-import repositories.SessionRepository
-import views.html.EuVatNumberView
 
-import scala.concurrent.Future
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import repositories.SessionRepository
+import views.html.vatEuDetails.EuVatNumberView
+import utils.FutureSyntax.FutureOps
 
 class EuVatNumberControllerSpec extends SpecBase with MockitoSugar {
 
-  val formProvider = new EuVatNumberFormProvider()
-  val form = formProvider()
+  private val euVatNumber: String = arbitraryEuVatNumber.sample.value
+  private val countryCode: String = euVatNumber.substring(0, 2)
+  private val country: Country = Country(countryCode, Country.euCountries.find(_.code == countryCode).head.name)
+  private val countryWithValidation = CountryWithValidationDetails.euCountriesWithVRNValidationRules.find(_.country.code == country.code).value
 
-  def onwardRoute = Call("GET", "/foo")
+  private val formProvider = new EuVatNumberFormProvider()
+  private val form: Form[String] = formProvider(country)
 
-  val validAnswer = 0
+  private lazy val euVatNumberRoute: String = routes.EuVatNumberController.onPageLoad(waypoints, countryIndex(0)).url
 
-  lazy val euVatNumberRoute = routes.EuVatNumberController.onPageLoad(NormalMode).url
+// TODO: update journey according to BR
+  private val updatedAnswers: UserAnswers = emptyUserAnswersWithVatInfo
+    .set(VatRegisteredInEuPage, true).success.value
+    .set(EuCountryPage(countryIndex(0)), country).success.value
+
 
   "EuVatNumber Controller" - {
 
     "must return OK and the correct view for a GET" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(updatedAnswers)).build()
 
       running(application) {
         val request = FakeRequest(GET, euVatNumberRoute)
@@ -56,14 +65,14 @@ class EuVatNumberControllerSpec extends SpecBase with MockitoSugar {
 
         val view = application.injector.instanceOf[EuVatNumberView]
 
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+        status(result) `mustBe` OK
+        contentAsString(result) `mustBe` view(form, waypoints, countryIndex(0), countryWithValidation)(request, messages(application)).toString
       }
     }
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(EuVatNumberPage, validAnswer).success.value
+      val userAnswers = updatedAnswers.set(EuVatNumberPage(countryIndex(0)), "answer").success.value
 
       val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
@@ -74,54 +83,58 @@ class EuVatNumberControllerSpec extends SpecBase with MockitoSugar {
 
         val result = route(application, request).value
 
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(validAnswer), NormalMode)(request, messages(application)).toString
+        status(result) `mustBe` OK
+        contentAsString(result) `mustBe` view(form.fill("answer"), waypoints, countryIndex(0), countryWithValidation)(request, messages(application)).toString
       }
     }
 
-    "must redirect to the next page when valid data is submitted" in {
+    "must save the answer and redirect to the next page when valid data is submitted" in {
 
       val mockSessionRepository = mock[SessionRepository]
 
-      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+      when(mockSessionRepository.set(any())) thenReturn true.toFuture
 
       val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        applicationBuilder(userAnswers = Some(updatedAnswers))
           .overrides(
-            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
-            bind[SessionRepository].toInstance(mockSessionRepository)
+            bind[SessionRepository].toInstance(mockSessionRepository),
           )
           .build()
 
       running(application) {
+
         val request =
           FakeRequest(POST, euVatNumberRoute)
-            .withFormUrlEncodedBody(("value", validAnswer.toString))
+            .withFormUrlEncodedBody(("value", euVatNumber))
 
         val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual onwardRoute.url
+        val expectedAnswers: UserAnswers = updatedAnswers
+          .set(EuVatNumberPage(countryIndex(0)), euVatNumber).success.value
+
+        status(result) `mustBe` SEE_OTHER
+        redirectLocation(result).value `mustBe` EuVatNumberPage(countryIndex(0)).navigate(waypoints, updatedAnswers, expectedAnswers).url
+        verify(mockSessionRepository, times(1)).set(eqTo(expectedAnswers))
       }
     }
 
     "must return a Bad Request and errors when invalid data is submitted" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(updatedAnswers)).build()
 
       running(application) {
         val request =
           FakeRequest(POST, euVatNumberRoute)
-            .withFormUrlEncodedBody(("value", "invalid value"))
+            .withFormUrlEncodedBody(("value", ""))
 
-        val boundForm = form.bind(Map("value" -> "invalid value"))
+        val boundForm = form.bind(Map("value" -> ""))
 
         val view = application.injector.instanceOf[EuVatNumberView]
 
         val result = route(application, request).value
 
-        status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+        status(result) `mustBe` BAD_REQUEST
+        contentAsString(result) `mustBe` view(boundForm, waypoints, countryIndex(0), countryWithValidation)(request, messages(application)).toString
       }
     }
 
@@ -134,26 +147,26 @@ class EuVatNumberControllerSpec extends SpecBase with MockitoSugar {
 
         val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        status(result) `mustBe` SEE_OTHER
+        redirectLocation(result).value `mustBe` JourneyRecoveryPage.route(waypoints).url
       }
     }
 
     "must redirect to Journey Recovery for a POST if no existing data is found" in {
 
-      val application = applicationBuilder(userAnswers = None).build()
+      val application: Application = applicationBuilder(userAnswers = None).build()
 
       running(application) {
         val request =
           FakeRequest(POST, euVatNumberRoute)
-            .withFormUrlEncodedBody(("value", validAnswer.toString))
+            .withFormUrlEncodedBody(("value", euVatNumber))
 
         val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        status(result) `mustBe` SEE_OTHER
+        redirectLocation(result).value `mustBe` JourneyRecoveryPage.route(waypoints).url
       }
     }
+
   }
 }
