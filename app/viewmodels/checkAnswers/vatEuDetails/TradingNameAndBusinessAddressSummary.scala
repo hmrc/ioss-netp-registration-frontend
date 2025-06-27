6141618a02d7fc7e6a282fa11b7b8eef7299ce99
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-06-27 10:42:55 +0100

    update TradingNameAndBusinessAddressSummary for view

--- app/viewmodels/checkAnswers/vatEuDetails/TradingNameAndBusinessAddressSummary.scala
+++ app/viewmodels/checkAnswers/vatEuDetails/TradingNameAndBusinessAddressSummary.scala
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package viewmodels.checkAnswers.vatEuDetails
 
 import controllers.routes
-import models.{CheckMode, UserAnswers}
+import models.{CheckMode, Index, UserAnswers}
+import pages.{CheckAnswersPage, Waypoints}
 import pages.vatEuDetails.TradingNameAndBusinessAddressPage
 import play.api.i18n.Messages
 import play.twirl.api.HtmlFormat
@@ -10,21 +27,33 @@ import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
 import viewmodels.govuk.summarylist.*
 import viewmodels.implicits.*
 
-object TradingNameAndBusinessAddressSummary  {
+object TradingNameAndBusinessAddressSummary {
 
-  def row(answers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
-    answers.get(TradingNameAndBusinessAddressPage).map {
-      answer =>
 
-      val value = HtmlFormat.escape(answer.field1).toString + "<br/>" + HtmlFormat.escape(answer.field2).toString
+  def row(
+           waypoints: Waypoints,
+           answers: UserAnswers,
+           countryIndex: Index,
+           sourcePage: CheckAnswersPage
+         )(implicit messages: Messages): Option[SummaryListRow] = {
+    answers.get(TradingNameAndBusinessAddressPage(countryIndex)).map { answer =>
 
-        SummaryListRowViewModel(
-          key     = "tradingNameAndBusinessAddress.checkYourAnswersLabel",
-          value   = ValueViewModel(HtmlContent(value)),
-          actions = Seq(
-            ActionItemViewModel("site.change", routes.TradingNameAndBusinessAddressController.onPageLoad(CheckMode).url)
-              .withVisuallyHiddenText(messages("tradingNameAndBusinessAddress.change.hidden"))
-          )
+      val value = Seq(
+        Some(HtmlFormat.escape(answer.line1).toString),
+        answer.line2.map(HtmlFormat.escape),
+        Some(HtmlFormat.escape(answer.townOrCity).toString),
+        answer.stateOrRegion.map(HtmlFormat.escape),
+        answer.postCode.map(HtmlFormat.escape)
+      ).flatten.mkString("<br/>")
+
+      SummaryListRowViewModel(
+        key = "fixedEstablishmentAddress.checkYourAnswersLabel",
+        value = ValueViewModel(HtmlContent(value)),
+        actions = Seq(
+          ActionItemViewModel("site.change", TradingNameAndBusinessAddressPage(countryIndex).changeLink(waypoints, sourcePage).url)
+            .withVisuallyHiddenText(messages("fixedEstablishmentAddress.change.hidden"))
         )
+      )
     }
+  }
 }
