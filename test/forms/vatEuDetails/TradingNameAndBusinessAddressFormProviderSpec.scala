ba6437823cf3f94bdfec84b2c7594921e061f24b
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-06-30 13:38:11 +0100

    add TradingNameAndBusinessAddressFormProvider tests

--- test/forms/vatEuDetails/TradingNameAndBusinessAddressFormProviderSpec.scala
+++ test/forms/vatEuDetails/TradingNameAndBusinessAddressFormProviderSpec.scala
@@ -1,18 +1,39 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package forms.vatEuDetails
 
 import forms.behaviours.StringFieldBehaviours
-import play.api.data.FormError
+import forms.validation.Validation.{commonTextPattern, postcodePattern}
+import models.{Country, InternationalAddress}
+import play.api.data.{Form, FormError}
 
 class TradingNameAndBusinessAddressFormProviderSpec extends StringFieldBehaviours {
 
-  val form = new TradingNameAndBusinessAddressFormProvider()()
+  private val country: Country = arbitraryCountry.arbitrary.sample.value
 
-  ".field1" - {
+  private val form: Form[InternationalAddress] = new TradingNameAndBusinessAddressFormProvider()(Some(country))
 
-    val fieldName = "field1"
-    val requiredKey = "tradingNameAndBusinessAddress.error.field1.required"
-    val lengthKey = "tradingNameAndBusinessAddress.error.field1.length"
-    val maxLength = 100
+  ".line1" - {
+
+    val fieldName: String = "line1"
+    val requiredKey: String = "fixedEstablishmentAddress.error.line1.required"
+    val lengthKey: String = "fixedEstablishmentAddress.error.line1.length"
+    val formatKey: String = "fixedEstablishmentAddress.error.line1.format"
+    val maxLength: Int = 35
 
     behave like fieldThatBindsValidData(
       form,
@@ -20,31 +41,106 @@ class TradingNameAndBusinessAddressFormProviderSpec extends StringFieldBehaviour
       stringsWithMaxLength(maxLength)
     )
 
-    behave like fieldWithMaxLength(
-      form,
-      fieldName,
-      maxLength = maxLength,
-      lengthError = FormError(fieldName, lengthKey, Seq(maxLength))
-    )
-
     behave like mandatoryField(
       form,
       fieldName,
       requiredError = FormError(fieldName, requiredKey)
+    )
+
+    behave like commonTextField(
+      form,
+      fieldName,
+      FormError(fieldName, formatKey, Seq(commonTextPattern)),
+      FormError(fieldName, lengthKey, Seq(maxLength)),
+      maxLength
     )
   }
 
-  ".field2" - {
+  ".line2" - {
 
-    val fieldName = "field2"
-    val requiredKey = "tradingNameAndBusinessAddress.error.field2.required"
-    val lengthKey = "tradingNameAndBusinessAddress.error.field2.length"
-    val maxLength = 100
+    val fieldName: String = "line2"
+    val lengthKey: String = "fixedEstablishmentAddress.error.line2.length"
+    val formatKey: String = "fixedEstablishmentAddress.error.line2.format"
+    val maxLength: Int = 35
 
     behave like fieldThatBindsValidData(
       form,
       fieldName,
       stringsWithMaxLength(maxLength)
+    )
+
+    behave like commonTextField(
+      form,
+      fieldName,
+      FormError(fieldName, formatKey, Seq(commonTextPattern)),
+      FormError(fieldName, lengthKey, Seq(maxLength)),
+      maxLength
+    )
+  }
+
+  ".townOrCity" - {
+
+    val fieldName: String = "townOrCity"
+    val requiredKey: String = "fixedEstablishmentAddress.error.townOrCity.required"
+    val lengthKey: String = "fixedEstablishmentAddress.error.townOrCity.length"
+    val formatKey: String = "fixedEstablishmentAddress.error.townOrCity.format"
+    val maxLength: Int = 35
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      stringsWithMaxLength(maxLength)
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+
+    behave like commonTextField(
+      form,
+      fieldName,
+      FormError(fieldName, formatKey, Seq(commonTextPattern)),
+      FormError(fieldName, lengthKey, Seq(maxLength)),
+      maxLength
+    )
+  }
+
+  ".stateOrRegion" - {
+
+    val fieldName: String = "stateOrRegion"
+    val lengthKey: String = "fixedEstablishmentAddress.error.stateOrRegion.length"
+    val formatKey: String = "fixedEstablishmentAddress.error.stateOrRegion.format"
+    val maxLength: Int = 35
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      stringsWithMaxLength(maxLength)
+    )
+
+    behave like commonTextField(
+      form,
+      fieldName,
+      FormError(fieldName, formatKey, Seq(commonTextPattern)),
+      FormError(fieldName, lengthKey, Seq(maxLength)),
+      maxLength
+    )
+  }
+
+  ".postCode" - {
+
+    val fieldName: String = "postCode"
+    val lengthKey: String = "fixedEstablishmentAddress.error.postCode.length"
+    val invalidKey: String = "fixedEstablishmentAddress.error.postCode.invalid"
+    val maxLength: Int = 40
+    val validData: String = "75023 CEDEX 01"
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      validData
     )
 
     behave like fieldWithMaxLength(
@@ -54,10 +150,11 @@ class TradingNameAndBusinessAddressFormProviderSpec extends StringFieldBehaviour
       lengthError = FormError(fieldName, lengthKey, Seq(maxLength))
     )
 
-    behave like mandatoryField(
-      form,
-      fieldName,
-      requiredError = FormError(fieldName, requiredKey)
-    )
+    "must not bind invalid postcode" in {
+
+      val invalidPostcode: String = "AB@% *JH"
+      val result = form.bind(Map(fieldName -> invalidPostcode)).apply(fieldName)
+      result.errors mustBe Seq(FormError(fieldName, invalidKey, Seq(postcodePattern)))
+    }
   }
 }
