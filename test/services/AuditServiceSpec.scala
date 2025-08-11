c0950c3a58878fe71260480f124d0c6d5ee05c2d
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-08-11 15:56:06 +0100

    replace audit test with sendAudit test

--- test/services/AuditServiceSpec.scala
+++ test/services/AuditServiceSpec.scala
@@ -17,7 +17,8 @@
 package services
 
 import config.FrontendAppConfig
-import models.audit.{DeclarationSigningAuditModel, DeclarationSigningAuditType, SubmissionResult}
+import models.audit.SubmissionResult
+import models.requests.DataRequest
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{reset, times, verify, when}
 import org.scalatest.BeforeAndAfterEach
@@ -26,7 +27,7 @@ import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatestplus.mockito.MockitoSugar
 import play.api.test.FakeRequest
-import testutils.RegistrationData.emptyUserAnswers
+import testutils.RegistrationData.{emptyUserAnswers, intermediaryNumber, userAnswersId}
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.audit.http.connector.{AuditConnector, AuditResult}
 
@@ -42,23 +43,28 @@ class AuditServiceSpec extends AnyFreeSpec with MockitoSugar with ScalaFutures w
     reset(auditConnector)
   }
 
-  ".audit" - {
-
-    "must send Extended Event for create" in {
+  ".sendAudit" - {
+    "must send Extended Event for create declaration" in {
       when(auditConnector.sendExtendedEvent(any())(any(), any())) thenReturn Future.successful(AuditResult.Success)
 
       val service = new AuditService(mockAppConfig, auditConnector)
+      val testSubmissionResult = SubmissionResult.Failure
+      val testSubmittedDeclarationPageBody = "test-declaration-body"
 
-      service.audit(DeclarationSigningAuditModel(
-        declarationSigningAuditType = DeclarationSigningAuditType.CreateDeclaration,
-        credId = "test-cred-Id",
-        userAgent = "test-userAgent",
-        userAnswers =  emptyUserAnswers,
-        submissionResult = SubmissionResult.Success,
-        submittedDeclarationPageBody = "test-submittedDeclarationPageBody"
-      ))(hc, FakeRequest("POST", "test"))
+
+      implicit val dataRequest: DataRequest[_] = DataRequest(
+          FakeRequest("POST", "/test-path"),
+          userAnswersId,
+          emptyUserAnswers,
+          intermediaryNumber
+        )
+
+      service.sendAudit(
+        testSubmissionResult,
+        testSubmittedDeclarationPageBody
+      )
+
       verify(auditConnector, times(1)).sendExtendedEvent(any())(any(), any())
-
     }
   }
 }
