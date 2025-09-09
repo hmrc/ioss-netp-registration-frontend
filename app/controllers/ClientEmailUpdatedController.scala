4dc2097aa49be3219d26753fa7ff0d12424413be
Tayamul <100432502+Tayamul@users.noreply.github.com>
2025-09-09 15:20:07 +0100

    VEI-324: Build acknowledgement page to confirm successful email update.

--- app/controllers/ClientEmailUpdatedController.scala
+++ app/controllers/ClientEmailUpdatedController.scala
@@ -1,24 +1,64 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
-import controllers.actions._
+import config.FrontendAppConfig
+import connectors.RegistrationConnector
+import controllers.actions.*
+import logging.Logging
+
 import javax.inject.Inject
 import play.api.i18n.{I18nSupport, MessagesApi}
-import pages.{Waypoint, Waypoints}
+import pages.{BusinessContactDetailsPage, Waypoints}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.ClientEmailUpdatedView
 
+import scala.concurrent.ExecutionContext
+
 class ClientEmailUpdatedController @Inject()(
                                        override val messagesApi: MessagesApi,
-                                       identify: IdentifierAction,
-                                       getData: DataRetrievalAction,
-                                       requireData: DataRequiredAction,
+                                       cc: AuthenticatedControllerComponents,
                                        val controllerComponents: MessagesControllerComponents,
+                                       frontendAppConfig: FrontendAppConfig,
+                                       registrationConnector: RegistrationConnector,
                                        view: ClientEmailUpdatedView
-                                     ) extends FrontendBaseController with I18nSupport {
+                                     )(implicit ec: ExecutionContext)
+  extends FrontendBaseController with I18nSupport with Logging with GetOrganisationOrBusinessName {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad(waypoints: Waypoints, journeyId: String): Action[AnyContent] = cc.identify.async {
     implicit request =>
-      Ok(view())
+
+        registrationConnector.getPendingRegistration(journeyId).map {
+          case Right(pendingRegistration) =>
+
+              val clientCompanyName = getClientCompanyName(pendingRegistration)
+
+              val emailAddress = pendingRegistration.userAnswers.get(BusinessContactDetailsPage).get.emailAddress
+
+              val dashboardUrl = frontendAppConfig.intermediaryYourAccountUrl
+
+              Ok(view(clientCompanyName, emailAddress, dashboardUrl))
+
+          case Left(errors) =>
+            val message: String = s"Received an unexpected error when trying to retrieve a pending registration for the given journey ID: $errors."
+            val exception: Exception = new Exception(message)
+            logger.error(exception.getMessage, exception)
+            throw exception
+      }
   }
 }
--- test/controllers/ClientEmailUpdatedControllerSpec.scala
+++ test/controllers/ClientEmailUpdatedControllerSpec.scala
@@ -1,27 +1,90 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.SpecBase
+import connectors.RegistrationConnector
+import models.responses.InternalServerError
+import models.{BusinessContactDetails, SavedPendingRegistration}
+import org.mockito.ArgumentMatchers.{any, eq as eqTo}
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatestplus.mockito.MockitoSugar
+import pages.BusinessContactDetailsPage
+import play.api.inject.bind
 import play.api.test.FakeRequest
-import play.api.test.Helpers._
-import views.html.ClientEmailUpdatedView
+import play.api.test.Helpers.*
+import utils.FutureSyntax.FutureOps
 
-class ClientEmailUpdatedControllerSpec extends SpecBase {
+class ClientEmailUpdatedControllerSpec extends SpecBase with MockitoSugar {
+
+  private val savedPendingRegistration: SavedPendingRegistration = arbitrarySavedPendingRegistration.arbitrary.sample.value
+  override val journeyId: String = savedPendingRegistration.journeyId
+
+  val newEmailAddress: String = "new@email.com"
+
+  val mockRegistrationConnector: RegistrationConnector = mock[RegistrationConnector]
 
   "ClientEmailUpdated Controller" - {
 
     "must return OK and the correct view for a GET" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val businessContactDetails =
+        BusinessContactDetails(fullName = "", telephoneNumber = "", emailAddress = newEmailAddress)
+
+      val updatedUserAnswers = savedPendingRegistration.userAnswers
+        .set(BusinessContactDetailsPage, businessContactDetails).success.value
+
+      val updatedPendingReg = savedPendingRegistration.copy(userAnswers = updatedUserAnswers)
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[RegistrationConnector].toInstance(mockRegistrationConnector))
+        .build()
+
+      when(mockRegistrationConnector.getPendingRegistration(eqTo(journeyId))(any()))
+        .thenReturn(Right(updatedPendingReg).toFuture)
 
       running(application) {
-        val request = FakeRequest(GET, routes.ClientEmailUpdatedController.onPageLoad().url)
+        val request = FakeRequest(GET, routes.ClientEmailUpdatedController.onPageLoad(waypoints, journeyId).url)
 
         val result = route(application, request).value
 
-        val view = application.injector.instanceOf[ClientEmailUpdatedView]
-
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view()(request, messages(application)).toString
+        contentAsString(result) must include(newEmailAddress)
+        verify(mockRegistrationConnector, times(1)).getPendingRegistration(eqTo(journeyId))(any())
+      }
+    }
+
+    "must throw an exception and log an error when the connector fails to return a pending registration" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[RegistrationConnector].toInstance(mockRegistrationConnector))
+        .build()
+
+      when(mockRegistrationConnector.getPendingRegistration(eqTo(journeyId))(any()))
+        .thenReturn(Left(InternalServerError).toFuture)
+
+      running(application) {
+        val request = FakeRequest(GET, routes.ClientEmailUpdatedController.onPageLoad(waypoints, journeyId).url)
+
+        val result = route(application, request).value
+
+        intercept[Exception] {
+          status(result)
+        }
       }
     }
   }
