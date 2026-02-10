fc60741f3f952e239a073998476ebf49a293d304
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2026-02-10 19:52:22 +0000

    VEI-506 | Add functionality to revalidate all eu details with specs. WIP.

--- app/services/core/CoreSavedAnswersRevalidationService.scala
+++ app/services/core/CoreSavedAnswersRevalidationService.scala
@@ -7,7 +7,10 @@ import models.Country
 import models.core.Match
 import models.domain.VatCustomerInfo
 import models.requests.DataRequest
+import models.vatEuDetails.EuDetails
+import pages.vatEuDetails.HasFixedEstablishmentPage
 import pages.{ClientCountryBasedPage, ClientTaxReferencePage, ClientUtrNumberPage, ClientVatNumberPage, ClientsNinoNumberPage, Waypoints}
+import queries.euDetails.AllEuDetailsQuery
 import uk.gov.hmrc.domain.Vrn
 import uk.gov.hmrc.http.HeaderCarrier
 import utils.FutureSyntax.FutureOps
@@ -25,7 +28,6 @@ class CoreSavedAnswersRevalidationService @Inject()(
   // ClientsNinoNumberPage -> searchTraderId
   // ClientTaxReferencePage -> searchForeignTaxReference
 
-  // USE THE MODELS FOR THESE -> LOOK AT IOSS-REG
   // EuTaxReferencePage -> searchEuTaxId
   // EuVatNumberPage -> searchEuVrn
   // PreviousIossNumberPage -> searchScheme
@@ -35,14 +37,17 @@ class CoreSavedAnswersRevalidationService @Inject()(
     request.userAnswers.get(ClientVatNumberPage) match {
       case Some(value) =>
         revalidateUKVrn(waypoints, Vrn(value))
+
       case _ =>
         request.userAnswers.get(ClientUtrNumberPage) match {
           case Some(value) =>
             revalidateTraderId(value)
+
           case _ =>
             request.userAnswers.get(ClientsNinoNumberPage) match {
               case Some(value) =>
                 revalidateTraderId(value)
+
               case _ =>
                 request.userAnswers.get(ClientTaxReferencePage) match {
                   case Some(value) =>
@@ -56,8 +61,17 @@ class CoreSavedAnswersRevalidationService @Inject()(
                     revalidateForeignTaxReference(value, country.code)
 
                   case _ =>
-                    // TODO -> Call methods to iterate through List[EuDetails] and List[PrevReg]
-                    None.toFuture // TODO -> Must return None when all avenues exhausted and user can continue with reg
+                    request.userAnswers.get(HasFixedEstablishmentPage) match {
+                      case Some(true) =>
+                        val euDetails = request.userAnswers.get(AllEuDetailsQuery).getOrElse(List.empty)
+                        // TODO -> Should this be raw details as user may have only saved partial EuDetails?
+                        checkAllEuDetails(euDetails)
+
+                      case _ =>
+                        // TODO -> Call method to iterate through List[PrevReg]
+                        // TODO -> Must return None when all avenues exhausted and user can continue with reg
+                        None.toFuture
+                    }
                 }
             }
         }
@@ -85,6 +99,51 @@ class CoreSavedAnswersRevalidationService @Inject()(
                                              countryCode: String
                                            )(implicit hc: HeaderCarrier, request: DataRequest[_]): Future[Option[String]] = {
     coreRegistrationValidationService.searchForeignTaxReference(foreignReferenceNumber, countryCode).flatMap { maybeActiveMatch =>
+      activeMatchRedirectUrl(maybeActiveMatch)
+    }
+  }
+
+  private def checkAllEuDetails(allEuDetails: List[EuDetails])(implicit hc: HeaderCarrier, request: DataRequest[_]): Future[Option[String]] = {
+    allEuDetails match {
+      case ::(currentEuDetails, remaining) =>
+        revalidateEuDetails(currentEuDetails, currentEuDetails.euVatNumber).flatMap {
+          case Some(urlString) => Some(urlString).toFuture
+          case _ => checkAllEuDetails(remaining)
+        }
+
+      case Nil => None.toFuture
+    }
+  }
+
+  private def revalidateEuDetails(
+                                   euDetails: EuDetails,
+                                   euVatNumber: Option[String]
+                                 )(implicit hc: HeaderCarrier, request: DataRequest[_]): Future[Option[String]] = {
+    euVatNumber match {
+      case Some(euVrn) =>
+        revalidateEuVrn(euVrn, euDetails.euCountry.code)
+
+      case _ => euDetails.euTaxReference match {
+        case Some(euTaxReference) =>
+          revalidateEuTaxId(euTaxReference, euDetails.euCountry.code)
+
+        case _ =>
+          val errorMessage: String = s"$euDetails has neither a euVatNumber or euTaxReference."
+          logger.error(errorMessage)
+          val exception = IllegalStateException(errorMessage)
+          throw exception
+      }
+    }
+  }
+
+  private def revalidateEuTaxId(euTaxReference: String, countryCode: String)(implicit hc: HeaderCarrier, request: DataRequest[_]) = {
+    coreRegistrationValidationService.searchEuTaxId(euTaxReference, countryCode).flatMap { maybeActiveMatch =>
+      activeMatchRedirectUrl(maybeActiveMatch)
+    }
+  }
+
+  private def revalidateEuVrn(euVrn: String, countryCode: String)(implicit hc: HeaderCarrier, request: DataRequest[_]) = {
+    coreRegistrationValidationService.searchEuVrn(euVrn, countryCode).flatMap { maybeActiveMatch =>
       activeMatchRedirectUrl(maybeActiveMatch)
     }
   }
