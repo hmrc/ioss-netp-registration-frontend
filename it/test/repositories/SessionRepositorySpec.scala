9529e292d48922ec9f69db078520c26b957166dc
Dimitris Skoumpourdis <19231661+dskoumpourdis@users.noreply.github.com>
2026-07-09 09:08:07 +0100

    VEI-882 | Ensure using correct encryption. (#147)
    * VEI-882 | Ensure using correct encryption.
    
    * VEI-882 | Ensure using correct encryption.
    
    * VEI-882 | Ensure using correct encryption.

--- it/test/repositories/SessionRepositorySpec.scala
+++ it/test/repositories/SessionRepositorySpec.scala
@@ -47,7 +47,7 @@ class SessionRepositorySpec
 
   private val mockConfiguration = mock[Configuration]
   private val mockConfig = mock[Config]
-  private val mockEncryptionService: EncryptionService = new EncryptionService(mockConfiguration)
+  private val mockEncryptionService: EncryptionService = mock[EncryptionService]
   private val encryptor = new UserAnswersEncryptor(mockAppConfig, mockEncryptionService)
   private val secretKey: String = "VqmXp7yigDFxbCUdDdNZVIvbW6RgPNJsliv6swQNCL8="
 
@@ -61,6 +61,11 @@ class SessionRepositorySpec
   when(mockConfiguration.underlying) thenReturn mockConfig
   when(mockConfig.getString(any())) thenReturn secretKey
   when(mockAppConfig.encryptionKey) thenReturn secretKey
+
+  private val encryptedUserAnswersData = "WovNVJUSKWKgpi/IrP3cgnx1COboOHM/0XqRS8XWpoD0Gj2Ng+DxqPUEXnEP"
+
+  when(mockEncryptionService.encryptField(any())) thenReturn encryptedUserAnswersData
+  when(mockEncryptionService.decryptField(any())) thenReturn userAnswers.data.toString
 
   ".set" - {
 
